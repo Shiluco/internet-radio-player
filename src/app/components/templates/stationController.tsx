d1ad7cd3fc41bf1bbab673d14e01c1bd879112a2
Shiluco <hirap1939@gmail.com>
2024-10-28 16:53:28 +0900

    refacor:スライダーの色変更

--- src/app/components/templates/stationController.tsx
+++ src/app/components/templates/stationController.tsx
@@ -7,14 +7,13 @@ const AudioController = () => {
   const { stations, currentStation, setCurrentStation } = useStationStore();
   const { setIsPlaying } = useMediaControlStore();
 
-  // 型を指定
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setIsPlaying(false); // 再生中の場合は停止
-    setCurrentStation(Number(event.target.value)); // Numberに変換してセット
+    setIsPlaying(false);
+    setCurrentStation(Number(event.target.value));
   };
 
   return (
-    <div className="h-[2vh] bg-gray-100 ">
+    <div className="h-[2vh] bg-gray-100">
       <div className="w-[100vw] mx-auto">
         <input
           className="w-full appearance-none h-1 bg-black rounded-lg overflow-hidden focus:outline-none focus:ring-2 focus:ring-indigo-900"
@@ -27,27 +26,27 @@ const AudioController = () => {
           aria-label="Slider for selecting a value"
         />
       </div>
+
+      <style jsx>{`
+        #slider::-webkit-slider-thumb {
+          appearance: none;
+          height: 16px;
+          width: 16px;
+          background-color: rgba(238, 120, 0, 1);
+          border-radius: 50%;
+          cursor: pointer;
+        }
+
+        #slider::-moz-range-thumb {
+          height: 16px;
+          width: 16px;
+          background-color: orange;
+          border-radius: 50%;
+          cursor: pointer;
+        }
+      `}</style>
     </div>
   );
 };
-
-<style jsx>{`
-  #slider::-webkit-slider-thumb {
-    appearance: none;
-    height: 16px; /* サムの高さ */
-    width: 16px; /* サムの幅 */
-    background-color: orange; /* サムの色 */
-    border-radius: 50%; /* 丸い形にする */
-    cursor: pointer;
-  }
-
-  #slider::-moz-range-thumb {
-    height: 16px;
-    width: 16px;
-    background-color: orange;
-    border-radius: 50%;
-    cursor: pointer;
-  }
-`}</style>;
 
 export default AudioController;
